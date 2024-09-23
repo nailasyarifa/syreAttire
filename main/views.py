91c8b77662f7a8dae19848cf8781fbc59278c165
nailasyarifa <nailasyarifa05@gmail.com>
2024-09-23 15:18:20 +0700

    progress tugas 4

--- main/views.py
+++ main/views.py
@@ -3,7 +3,17 @@ from .models import ItemEntry
 from .forms import ItemEntryForm
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+import datetime
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
+@login_required(login_url='/login')
 
 def show_main(request):
     items = ItemEntry.objects.all()  # Mengambil semua item dari database
@@ -11,6 +21,7 @@ def show_main(request):
     context = {
         'name': 'Your Name Here',  # Nama pengguna atau informasi lain
         'items': items,  # Daftar semua item pakaian
+        'last_login': request.COOKIES['last_login'],
         
     }
     return render(request, 'main.html', context)
@@ -41,3 +52,37 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = ItemEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register (request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request,'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+   if request.method == 'POST':
+      form = AuthenticationForm(data=request.POST)
+
+      if form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        response = HttpResponseRedirect(reverse("main:show_main"))
+        response.set_cookie('last_login', str(datetime.datetime.now()))
+        return response
+
+   else:
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
--- syreAttire/settings.py
+++ syreAttire/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost","http://127.0.0.1","https://pbp.cs.ui.ac.id/naila.syarifa/SyreAttire", "https://pbp.cs.ui.ac.id/naila.syarifa/SyreAttire"]
+
+
+
